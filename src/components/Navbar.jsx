d7c3ff33b42fc20a7f43276f55a33d5e1f853069
sauravDutt <sauravd069@gmail.com>
2022-08-16 11:34:55 +0530

    Just added some more stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
+import { motion } from "framer-motion";
 
 const Navbar = () => {
 
     return(
         <div className="navbar">
-
+            <motion.img alt="This is Logo" src={require('../img/logo.png')} whileHover={{scale: 1.01}} whileTap={{scale: 0.9}} className="logoImg"/>
         </div>
     );
 }
