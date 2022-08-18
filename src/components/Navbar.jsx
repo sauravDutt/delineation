ff2f8833563fc94d397d83e698e096c02b3334b3
sauravDutt <sauravd069@gmail.com>
2022-08-18 22:05:20 +0530

    Just added some more stf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
 
     return(
         <div className="navbar">
-            <motion.img alt="This is Logo" src={require('../img/logo.png')} whileHover={{scale: 1.01}} whileTap={{scale: 0.9}} className="logoImg"/>
+            <div className="innerNavbar">
+                <motion.img alt="This is Logo" src={require('../img/logo.png')} whileHover={{scale: 1.01}} whileTap={{scale: 0.9}} className="logoImg"/>
+            </div>
         </div>
     );
 }
