24edba830e0c8897b12dca0c15089636762e69e8
sauravDutt <sauravd069@gmail.com>
2022-08-19 00:31:23 +0530

    Just added some more stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,21 @@ const Navbar = () => {
         <div className="navbar">
             <div className="innerNavbar">
                 <motion.img alt="This is Logo" src={require('../img/logo.png')} whileHover={{scale: 1.01}} whileTap={{scale: 0.9}} className="logoImg"/>
+                <motion.button 
+                    className="navbarBtnEarth"
+                    whileHover={{scale: 1.1}} 
+                    whileTap={{scale: 0.5}}
+                >
+                 <img src={require('../img/earth.png')} alt="earth" className="earthImg"/>
+                </motion.button>
+                <motion.button 
+                    className="navbarBtn"
+                    whileHover={{scale: 1.1}} 
+                    whileTap={{scale: 0.5}}
+                >
+                 <i class="fa-solid fa-wallet"></i>
+                </motion.button>
+                
             </div>
         </div>
     );
