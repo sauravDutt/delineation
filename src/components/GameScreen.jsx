1ebce472aa667c8f7be0990baf28b7a2ebc8f341
sauravDutt <sauravd069@gmail.com>
2022-08-19 15:42:52 +0530

    Just added some more stuff

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -3,9 +3,12 @@ const GameScreen = () => {
 
     return(
         <div className="gameScreenOuter">
-        <div className="game-screen">
+            <div className="game-screen">
 
-        </div>
+            </div>
+            <h1><span className="letter">D</span>elineation</h1>
+            <h2><span className="letterTwo">C</span>lub.</h2>
+            
         </div>
     );
 }
