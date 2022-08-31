5075ae48666909966bd6b79ccd45e2f03bd9801b
sauravDutt <sauravd069@gmail.com>
2022-08-31 17:44:01 +0530

    Just added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let provider = new ethers.providers.Web3Provider(window.ethereum);
-let signer 
+// let provider = new ethers.providers.Web3Provider(window.ethereum);
+// let signer 
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
@@ -38,15 +38,15 @@ const closeModal = (modal) => {
     overlay.classList.remove('active');
 }
 
-// Connecting MataMask with the DAPP
-async function connectWallet() {
-    // MataMask requires Requesting permissions to connect to user's wallets.
-    await provider.send("eth_requestAccounts", []);
-    signer = await provider.getSigner();
+// // Connecting MataMask with the DAPP
+// async function connectWallet() {
+//     // MataMask requires Requesting permissions to connect to user's wallets.
+//     await provider.send("eth_requestAccounts", []);
+//     signer = await provider.getSigner();
 
-    const balance = await signer.getBalance();
-    const convertToEach = 1e18; 
+//     const balance = await signer.getBalance();
+//     const convertToEach = 1e18; 
 
-    console.log("Public Key : ", await signer.getAddress());
-    console.log("Balance : ", balance.toString() / convertToEach);
-}
+//     console.log("Public Key : ", await signer.getAddress());
+//     console.log("Balance : ", balance.toString() / convertToEach);
+// }
