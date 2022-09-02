8d9fb7381e8092b2bfe0f2be01ed3e8c564464ce
sauravDutt <sauravd069@gmail.com>
2022-09-02 18:17:21 +0530

    Just added some more stuff

---
+++ landing.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('mainCanvas');
+const c = canvas.getContext('2d');
+
+
+
+// var background = new Image();
+// background.src = "./img/background.png"
+
+// background.onload = () => {
+//     c.drawImage(background, 0, 0);
+// }
